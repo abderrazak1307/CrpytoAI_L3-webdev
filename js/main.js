f01d2658b2063ff5bdbed26f9d7c2f96cb42e4c7
Abderrazak BRAHIM <brahim.abderrazak1307@gmail.com>
2022-04-09 03:16:02 +0100

    Merging, Organizing, HTML5 WC Validating, Search scripted, Commented..etc

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 document.getElementById('navbutton').onclick = () => {
     document.getElementById('navbar').classList.toggle('show');
 }
+
+document.getElementById('search').onkeyup = (e) => {
+    if(e.keyCode === 13){
+        e.preventDefault();
+        location.href = 'search.html?q='+encodeURIComponent(e.target.value);
+    }
+}
